4b819d8d81927b1ac109afd0cc4a9da4758fd608
Nicolas <ncanete@trust.lat>
2023-07-29 17:50:09 -0400

    add theme manager implementation and test

--- src/main/kotlin/creational_pattern/singleton/ThemeManager.kt
+++ src/main/kotlin/creational_pattern/singleton/ThemeManager.kt
@@ -1,4 +1,45 @@
 package creational_pattern.singleton
 
-class ThemeManager {
+class ThemeManager private constructor() {
+    //Setting the constructor as private prevents the class from being instantiated directly
+    enum class Theme {
+        LIGHT,
+        DARK
+    }
+
+    private var currentTheme: Theme = Theme.LIGHT
+
+    companion object {
+        @Volatile
+        private  var instance: ThemeManager? = null
+
+        fun getInstance(): ThemeManager {
+            return  instance ?: synchronized(this) {
+                instance ?: ThemeManager().also { instance = it }
+            }
+        }
+    }
+
+    fun setTheme(theme: Theme) {
+        currentTheme = theme
+    }
+
+    fun getCurrentTheme() = currentTheme
+
+}
+
+class ThemeManagerTest {
+
+    fun testThemeManager() {
+        val themeManager1 = ThemeManager.getInstance()
+        val themeManager2 = ThemeManager.getInstance()
+
+        println(themeManager1)
+        println(themeManager2)
+        if (themeManager1 == themeManager2) println("singleton works fine") else println("something went wrong")
+    }
+}
+
+fun main() {
+    ThemeManagerTest().testThemeManager()
 }
